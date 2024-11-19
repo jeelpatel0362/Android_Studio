98f722d3dda1bd3f8c5a61ae23be45911e9d9036
jeelpatel0362 <jeelpatel0362@gmail.com>
2024-11-19 12:33:53 +0530

    form

--- app/src/main/java/com/example/form/MainActivity.java
+++ app/src/main/java/com/example/form/MainActivity.java
@@ -15,11 +15,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText nameEditText;
-    private EditText emailEditText;
-    private EditText ageEditText;
-    private EditText mobileEditText;
-    private Button submitButton;
 
 
     @Override
@@ -28,32 +23,5 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-
-        nameEditText = findViewById(R.id.edit_name);
-        emailEditText = findViewById(R.id.edit_email);
-        ageEditText = findViewById(R.id.edit_age);
-        mobileEditText = findViewById(R.id.edit_m_no);
-        submitButton = findViewById(R.id.submit_button);
-
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = nameEditText.getText().toString().trim();
-                String email = emailEditText.getText().toString().trim();
-                String age = ageEditText.getText().toString().trim();
-                String mobile = mobileEditText.getText().toString().trim();
-
-
-                if (name.isEmpty() || email.isEmpty() || age.isEmpty() || mobile.isEmpty()) {
-
-                    Toast.makeText(MainActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
-                } else {
-
-                    String message = "Name: " + name + "\nEmail: " + email + "\nAge: " + age + "\nMobile No: " + mobile;
-                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
-                }
-            }
-        });
     }
 }
